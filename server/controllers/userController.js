238012b4ddb91129b8e9e707fcdd87cd33718815
akash1723tripathi <akashtripathi.acad@gmail.com>
2025-09-12 23:29:34 +0530

    debug

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -53,10 +53,13 @@ export const login = async (req, res) =>{
         res.json({success: false, message: error.message})
     }
 }
+
+
 // Controller to check if user is authenticated
 export const checkAuth = (req, res)=>{
     res.json({success: true, user: req.user});
 }
+
 
 // Controller to update user profile details
 export const updateProfile = async (req, res)=>{
